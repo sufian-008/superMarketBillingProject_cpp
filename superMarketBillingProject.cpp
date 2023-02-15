2af7763e97ed821fdb3371f5e20ee2fa7ad5457a
mdrafin008 <mdrafin008@gmail.com>
2023-02-15 22:25:52 +0600

    seecond commit

--- superMarketBillingProject.cpp
+++ superMarketBillingProject.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<fstream>
 
+
+
 using namespace std;
+
+
 
 class shopping
 {
@@ -27,7 +31,7 @@ void shopping :: menu()
     m:
     int choice;
     string email;
-    string password;
+    int password;
 
     cout<<"\t\t\t\t***********************************\n";
     cout<<"\t\t\t\t                                   \n";
@@ -52,7 +56,7 @@ void shopping :: menu()
           cout<<"\t\t\t Password       \n";
           cin>>password;
 
-          if(email="mdrafin008@gmail.com"  && password="rafin"){
+          if(email=="mdrafin@gmail.com"  &&  password==1111){
              
              adminastration();
 
@@ -108,7 +112,7 @@ void shopping :: adminastration()
             edit();
             break;
          case 3:
-             rem();
+             remove();
              break;
 
          case 4:
@@ -150,6 +154,123 @@ void shopping :: buyer()
       goto m;
     
 }
+
+void shopping ::add(){
+  m:
+  fstream data;
+  int c;
+  int token =0;
+  float d;
+  float p;
+  string n;
+   
+
+  cout<<"\n\n\t\t\t Add new Product";
+  cout<<"\n\n\t Product code of the product";
+  cin>>pcode;
+  cout<<"\n\n\t Name of the product";
+  cin>>Pname;
+  cout<<"\n\n\t Price of the Product";
+  cin>>price;
+  cout<<"\n\n\t Discount on product";
+  cin>>dis;
+
+  data.open("database.txt",ios::in);
+  if(!data){
+    data.open("database.txt",ios::app|ios::out);
+    data<<" "<<pcode<<" "<<price<<" "<<dis<<"\n";
+  }
+  else{
+    data>>c>>n>>p>>d;
+   
+    while(!data.eof())
+
+    {
+      if(c == pcode){
+        
+      token++;
+
+      }
+         data>>c>>n>>p>>d;
+    }
+    data.close();
+  }
+    if(token == 1){
+      goto m;
+
+    }else{
+
+         data.open("database.txt", ios::app|ios::out);
+         data<<" "<<pcode<<" "<<Pname<<" "<<price<<" "<<dis<<"\n";
+         data.close();
+    }
+    cout<<"\n\n\t\t Record inserted !";
+
+}
+
+void shopping :: edit(){
+     m:
+  fstream data,data1;
+  int pkey;
+  int token =0;
+  int c;
+  float p;
+  float d;
+  string n;
+
+  cout<<"\n\t\t\t Modify the record";
+  cout<<"\n\t\t\t Product code";
+  cin>>pkey;
+
+  data.open("database.txt",ios::in);
+  if(!data){
+    cout<<"\n\nFiles dosen't";
+
+  }else{
+
+    data1.open("database.txt", ios::app|ios::out);
+    data>>pcode>>Pname>>price>>dis;
+
+    while(!data.eof()){
+      if(pkey ==pcode){
+        cout<<"\n\t\t Product new code";
+        cout<<"\n\t\tName of the product";
+        cin>>p;
+        cout<<"\n\t\t Discount";
+        cin>>d;
+        data1<<" "<<c<<" "<<n<<" "<<p<<" "<<d<<"\n";
+        data.close();
+
+      
+      }
+      else{
+        data>>c>>n>>p>>d;
+        while(!data.eof()){
+          if(c ==pcode){
+            token++;
+          }
+          data>>c>>n>>p>>d;
+        }
+        data.close();
+      }
+
+      if(token==1)
+        goto m;
+      else{
+        data.open("database.txt", ios::app|ios::out);
+        data<<" "<<pcode<<" "<<price<<" "<<dis<<"\n";
+        data.close();
+      }
+      
+     }
+cout<<"\n\n\t\tRecord inserted !";
+
+
+}
+}
+
+
+
 
 
 
